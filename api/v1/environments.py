f80cd0641b3d2131dc7a46a4810c3833657a6273
Aspect13 <ad13box@gmail.com>
2022-08-25 15:31:26 +0300

    fixing scopes for thresholds

--- api/v1/environments.py
+++ api/v1/environments.py
@@ -16,9 +16,7 @@ class API(Resource):
     def get(self, project_id: int):
         project = self.module.context.rpc_manager.call.project_get_or_404(project_id=project_id)
         query_result = UIReport.query.with_entities(UIReport.environment).filter(
-            and_(
-                UIReport.name == request.args.get("name"),
-                UIReport.project_id == project.id
-            )
+            UIReport.name == request.args.get("name"),
+            UIReport.project_id == project.id
         ).distinct(UIReport.environment).all()
         return [i[0] for i in query_result], 200
