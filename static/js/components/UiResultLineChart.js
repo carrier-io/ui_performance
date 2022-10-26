a6d79c209d5aba2132fac3f520e11922668d20fa
Vitalii_Kondratskyi <Vitalii_Kondratskyi@epam.com>
2022-10-26 21:25:11 +0300

    feature(Performance-ui || EPMTIGR-5150): - Added dotted lines;

--- static/js/components/UiResultLineChart.js
+++ static/js/components/UiResultLineChart.js
@@ -7,6 +7,8 @@ const UiResultLineChart = {
             legendList: [],
             selectedLegend: [],
             loading: false,
+            pagesCount: 0,
+            isShowDottedLine: true,
         }
     },
     mounted() {
@@ -14,6 +16,8 @@ const UiResultLineChart = {
     },
     watch: {
         metric(newValue, oldValue) {
+            this.lineChart.destroy();
+            this.lineChart = null;
             this.generateDatasets(newValue, true)
         }
     },
@@ -26,9 +30,39 @@ const UiResultLineChart = {
         }
     },
     methods: {
+        generateDottedDatasets() {
+            const countIndex = this.lineChartData[0].labels.length;
+            const dottedData = [];
+
+            for (let i = 0; i < countIndex; i++) {
+                const data = [];
+                this.lineChartData.forEach((page) => {
+                    if (!this.selectedLegend.includes(page.name)) return
+                    data.push({
+                        y: page.datasets[this.metric][i],
+                        x: page.labels[i]
+                    })
+                })
+                dottedData.push({
+                    borderDash: [10,5],
+                    data,
+                    label: `loop ${i}`
+                })
+            }
+            return dottedData;
+        },
         generateDatasets(metric, isUpdate = false) {
-            if (isUpdate) this.selectedLegend = [];
+            if (isUpdate) {
+                this.selectedLegend = [];
+                this.$refs['legendCbx'].forEach(cbx => {
+                    cbx.checked = true;
+                })
+                this.$refs['dottedLegendCbx'].checked = true
+                this.pagesCount = 0;
+            }
+
             const datasets = this.lineChartData.map((page, index) => {
+                this.pagesCount++;
                 this.selectedLegend.push(page.name)
                 const ds = {
                     backgroundColor: barColors[index],
@@ -46,12 +80,8 @@ const UiResultLineChart = {
                     data,
                 }
             })
-            if (this.lineChart) {
-                this.lineChart.data.datasets = datasets;
-                this.lineChart.update();
-            } else {
-                this.drawCanvas(datasets);
-            }
+            const mixedDatasets = [...datasets, ...this.generateDottedDatasets()];
+            this.drawCanvas(mixedDatasets);
         },
         drawCanvas(datasets) {
             const ctx = document.getElementById("linechart");
@@ -81,19 +111,38 @@ const UiResultLineChart = {
                 }
             });
             this.lineChart = chart
-            this.legendList = chart.options.plugins.legend.labels.generateLabels(chart)
+            this.legendList = chart.options.plugins.legend.labels.generateLabels(chart).slice(0, this.pagesCount);
+        },
+        combineDatasets() {
+            this.lineChart.data.datasets = [...this.lineChart.data.datasets.slice(0, this.pagesCount),
+                ...this.generateDottedDatasets()];
+            this.lineChart.data.datasets.forEach((ds, i) => {
+                if (i >= this.pagesCount) {
+                    this.lineChart.getDatasetMeta(i).hidden = !this.isShowDottedLine;
+                }
+            })
+        },
+        selectDottedLegend({ target: { checked }}) {
+            let hidden = checked;
+            this.isShowDottedLine = checked;
+            this.lineChart.data.datasets.forEach((ds, i) => {
+                if (i >= this.pagesCount) {
+                    this.lineChart.getDatasetMeta(i).hidden = !hidden;
+                }
+            })
+            this.lineChart.update();
         },
         selectLegend(legend) {
             let hidden = !this.lineChart.getDatasetMeta(legend.datasetIndex).hidden;
             this.selectedLegend = hidden
                 ? this.selectedLegend.filter(lg => lg !== legend.text)
                 : [...this.selectedLegend, legend.text];
-
             this.lineChart.data.datasets.forEach((ds, i) => {
                 if (this.lineChart.getDatasetMeta(legend.datasetIndex).label === ds.label) {
                     this.lineChart.getDatasetMeta(i).hidden = hidden;
                 }
             })
+            this.combineDatasets();
             this.lineChart.update()
         },
         selectAll({ target }) {
@@ -109,6 +158,10 @@ const UiResultLineChart = {
                     cbx.checked = !hidden;
                 })
             })
+            if (!hidden) {
+                this.combineDatasets();
+            }
+
             this.lineChart.update()
         }
     },
@@ -153,6 +206,24 @@ const UiResultLineChart = {
                                 :style="{'--cbx-color': legend.fillStyle}"
                                 type="checkbox">
                             <span class="w-100 d-inline-block">{{ legend.text }}</span>
+                        </label>
+                    </div>
+                </div>
+                <hr class="my-0">
+                <div id="linechart-group-legend"
+                     class="custom-chart-legend d-flex flex-column px-3 py-3"
+                >
+                    <div class="d-flex float-left mr-3">
+                        <label
+                            class="mb-0 w-100 d-flex align-items-center custom-checkbox custom-checkbox__multicolor">
+                            <input
+                                class="mx-2 custom__checkbox"
+                                checked="true"
+                                ref="dottedLegendCbx"
+                                @change="selectDottedLegend($event)"
+                                :style="{'--cbx-color': '#dadada'}"
+                                type="checkbox">
+                            <span class="w-100 d-inline-block">Show dotted Line</span>
                         </label>
                     </div>
                 </div>
