20052d7e5b8cd406b92462ad3d239d59fef9833b
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2025-01-28 12:36:38 +0200

    update containers list

--- constants.py
+++ constants.py
@@ -2,6 +2,7 @@ from tools import constants as c
 
 RUNNER_MAPPING = {
     'Lighthouse-NPM_V12': f'getcarrier/observer-lighthouse-nodejs:{c.CURRENT_RELEASE}-alpine_v12',
+    'Lighthouse-NPM_V11': f'getcarrier/observer-lighthouse-nodejs:{c.CURRENT_RELEASE}-alpine_v11',
     'Lighthouse-Nodejs': f'getcarrier/observer-lighthouse-nodejs:{c.CURRENT_RELEASE}',
     'Sitespeed v22': f'getcarrier/observer-browsertime:{c.CURRENT_RELEASE}-22.0.0',
     'Sitespeed v33': f'getcarrier/observer-browsertime:{c.CURRENT_RELEASE}-33.0.0'
