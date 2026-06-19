518904b1e524aec3353c36678984aa6bad032499
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2026-06-19 15:44:52 +0300

    add baseline tag

--- api/v1/baseline.py
+++ api/v1/baseline.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from flask import request, make_response
+from flask import request
 from ...models.ui_baseline import UIBaseline
 from ...models.ui_report import UIReport
 from tools import auth
@@ -8,6 +8,7 @@ from tools import auth
 class API(Resource):
     url_params = [
         '<int:project_id>',
+        '<int:project_id>/<int:report_id>',
     ]
 
     def __init__(self, module):
@@ -58,4 +59,39 @@ class API(Resource):
             report_id=report.id,
         )
         baseline.insert()
+        added_tag = report.add_tags([{'title': 'Baseline', 'hex': 'f54290'}])
+        if added_tag:
+            other_reports = UIReport.query.filter(
+                UIReport.project_id == project.id,
+                UIReport.name == report.name,
+                UIReport.environment == report.environment,
+                UIReport.id != report_id
+            ).all()
+            for other_report in other_reports:
+                if other_report.is_baseline_report:
+                    other_report.delete_tags(['Baseline'])
         return baseline.to_json(), 200
+
+    @auth.decorators.check_api({
+        "permissions": ["performance.ui_performance.baseline.delete"],
+        "recommended_roles": {
+            "default": {"admin": True, "editor": False, "viewer": False},
+            "administration": {"admin": True, "editor": False, "viewer": False},
+        }
+    })
+    def delete(self, project_id: int, report_id: int):
+        project = self.module.context.rpc_manager.call.project_get_or_404(
+            project_id=project_id)
+        report: UIReport = UIReport.query.filter(
+            UIReport.project_id == project.id,
+            UIReport.id == report_id
+        ).first()
+        if not report:
+            return 'Not found', 404
+        UIBaseline.query.filter(
+            UIBaseline.project_id == project.id,
+            UIBaseline.test == report.name,
+            UIBaseline.environment == report.environment
+        ).delete()
+        report.delete_tags(['Baseline'])
+        return {"message": "baseline was deleted"}, 204
